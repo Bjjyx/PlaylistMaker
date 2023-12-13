053493e4cbe2b9d878f81cd3394e8745175ca00a
Bjjyx <135358351+Bjjyx@users.noreply.github.com>
2023-12-13 12:57:42 +0300

    Коммит для сохранения.
    Добавлен код для сохранения написанного пользователем текста в поле editText
    
    Save commit.
    
    search_activity has been made
    Added code to save user's text in the editText

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -11,6 +11,24 @@ import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
 
 class SearchActivity : AppCompatActivity() {
+
+    private var savedString: CharSequence = STRING_DEF
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SAVED_STRING, savedString.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedString = savedInstanceState.getString(SAVED_STRING, STRING_DEF)
+    }
+
+    companion object {
+        const val SAVED_STRING = "SAVED_STRING"
+        const val STRING_DEF = ""
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
@@ -35,6 +53,7 @@ class SearchActivity : AppCompatActivity() {
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 clearButton.visibility = clearButtonVisibility(s)
+                saveString(s)
             }
 
             override fun afterTextChanged(s: Editable?) {
@@ -42,8 +61,15 @@ class SearchActivity : AppCompatActivity() {
             }
         }
 
+        searchEditText.setText(savedString)
         searchEditText.addTextChangedListener(searchTextWatcher)
 
+    }
+
+    private fun saveString(s: CharSequence?){
+        if (!s.isNullOrEmpty()){
+            savedString = s
+        }
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int{
